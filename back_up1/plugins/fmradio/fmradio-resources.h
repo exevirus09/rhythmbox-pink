03c54c030a79ce4a3f72362bcefc977b539e5348
exevirus09 <exevirus099@gmail.com>
2026-08-30 16:07:49 -0600

    Add files via upload

---
+++ back_up1/plugins/fmradio/fmradio-resources.h
@@ -0,0 +1,7 @@
+#ifndef __RESOURCE_fmradio_H__
+#define __RESOURCE_fmradio_H__
+
+#include <gio/gio.h>
+
+G_GNUC_INTERNAL GResource *fmradio_get_resource (void);
+#endif
---
+++ back_up1/plugins/generic-player/generic-player-resources.h
@@ -0,0 +1,7 @@
+#ifndef __RESOURCE_generic_player_H__
+#define __RESOURCE_generic_player_H__
+
+#include <gio/gio.h>
+
+G_GNUC_INTERNAL GResource *generic_player_get_resource (void);
+#endif

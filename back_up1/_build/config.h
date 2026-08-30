10c31ecadabe3210c8738f24d10daebfbe3cbfde
exevirus09 <exevirus099@gmail.com>
2026-08-30 16:23:45 -0600

    Add files via upload

---
+++ back_up1/_build/config.h
@@ -0,0 +1,42 @@
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+/* Manually converted for use by Meson. */
+
+/* GLib version requirements */
+#define GLIB_VERSION_MIN_REQUIRED GLIB_VERSION_2_66
+#define GLIB_VERSION_MAX_ALLOWED GLIB_VERSION_2_66
+
+/* Define to the gettext package name. */
+#define GETTEXT_PACKAGE "rhythmbox"
+
+/* Define if you have GNU fwrite_unlocked */
+#define HAVE_GNU_FWRITE_UNLOCKED
+
+/* Use GUdev for hardware detection */
+#define HAVE_GUDEV 1
+
+/* define if Multimedia Keys are enabled */
+/* #undef HAVE_MMKEYS */
+
+/* Name of package */
+#define PACKAGE "rhythmbox"
+
+/* Define if libsecret support is enabled */
+#define WITH_LIBSECRET
+
+/* Define if you have cfmakeraw */
+#define HAVE_CFMAKERAW
+
+/* Define to the version of this package. */
+#define VERSION "3.5.1"
+
+/* Define if prctl is available. */
+#define HAVE_PRCTL
+
+/* Define if pthread_getname_np is available. */
+#define HAVE_PTHREAD_GETNAME_NP
+
+/* Define if libmtp support is enabled */
+#define WITH_LIBMTP 1
+
+/* Define if using girepository 2.0 */
+/* #undef USE_GIREPOSITORY2 */
